7e1e2d9e238b2d0e009219313c2fe4fe4322b41a
Peter Nemere <peter.nemere@qut.edu.au>
2026-06-01 14:17:01 +1000

    Removed duplicate logging

--- runner/main.go
+++ runner/main.go
@@ -21,7 +21,7 @@ func main() {
 		log.Fatalf("Failed to read %v: %v. Error %v", jobrunner.EnvNodeIndexName, nodeIdxStr, err)
 	}
 
-	fmt.Println("PIXLISE Job Runner Starting, job path: s3://%v/%v, node index: %v...", bucket, jobPath, nodeIdx)
+	fmt.Printf("PIXLISE Job Runner Starting, job path: s3://%v/%v, node index: %v...\n", bucket, jobPath, nodeIdx)
 
 	// Get a session for the bucket region
 	sess, err := awsutil.GetSession()
